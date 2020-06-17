eb7d3ded5ec416399b3aad0f1b94a4fbce409273
Ibrahim Sheikh <ibrahim.sheikh@ctrends-software.com>
2020-06-18 00:44:09 +0600

    migration tested: cool

---
+++ database/migrations/2020_06_17_181936_create_persons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('email');
+            $table->integer('phone');
+            $table->integer('age');
+            $table->boolean('is_alive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
